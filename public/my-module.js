402b60478ce80e56d2a1511d4fbe984d3064d7b0
NeilMoras <78868724+NeilMoras@users.noreply.github.com>
2021-06-01 12:04:29 -0400

    Add files via upload
    test2

--- public/my-module.js
+++ public/my-module.js
@@ -1,7 +1,6 @@
-import{theShire,buckland, HobbitSettlement} from './another-module.js';
-
-export {theShire, statement};
-
+import{ theShire, buckland, HobbitSettlement } from './another-module.js';
 
 const bree = new HobbitSettlement('Bree', 473, 'Atanatári who did not reach Beleriand');
 const statement = () => `${theShire.name} was founded ${buckland.founding - theShire.founding} years before ${buckland.name}.`;
+
+export { statement, theShire };
